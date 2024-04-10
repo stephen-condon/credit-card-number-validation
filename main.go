0738c1b5f5d1dbf1898ee3a8483db60e23a02611
Stephen Condon <scondon87@gmail.com>
2024-04-09 21:40:51 -0500

    Added usgae example to main.go

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"creditcardvalidator/validator"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	accountNumber := "1789372997"
+	newCreditCardNumber := validator.ConstructCreditCardNumber(accountNumber)
+	fmt.Printf("Account Number: %s; CreditCardNumber: %s", accountNumber, newCreditCardNumber)
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Println("Validate Credit Card Number:")
+	isValid := validator.Validate(newCreditCardNumber)
+	if isValid {
+		fmt.Println("Credit Card Number is valid!")
+	} else {
+		fmt.Println("Credit Card Number is invalid!")
+	}
 }
